65142e9157f893d5f807cc997f3173b2827367df
georgie123 <georges.hinot@gmail.com>
2024-06-06 08:16:23 +0200

    Standard update

---
+++ _params.py
@@ -0,0 +1,67 @@
+
+import os
+from termcolor import colored
+import sys
+
+############################################# DIRECTORIES
+pycharmDirectoryGeorgesSamsungC = r'C:/Users/georg/PycharmProjects/'
+pycharmDirectoryGeorgesDesktopD = r'C:/Users/Georges/PycharmProjects/'
+pycharmDirectory = ''
+accessFile = ''
+
+if os.path.isdir(pycharmDirectoryGeorgesSamsungC):
+    print(colored('\nWe work from ' + pycharmDirectoryGeorgesSamsungC, 'green'))
+    pycharmDirectory = pycharmDirectoryGeorgesSamsungC
+    accessFile = pycharmDirectory + 'gjcY8d4q6mvC2WXy.ztxt'
+    sys.path.append(pycharmDirectory + r'Private/JoomlaMigration_HG2')
+
+if os.path.isdir(pycharmDirectoryGeorgesDesktopD):
+    print(colored('\nWe work from ' + pycharmDirectoryGeorgesDesktopD, 'green'))
+    pycharmDirectory = pycharmDirectoryGeorgesDesktopD
+    accessFile = pycharmDirectory + 'gjcY8d4q6mvC2WXy.ztxt'
+    sys.path.append(pycharmDirectory + r'Private/JoomlaMigration_HG2')
+######################################### DIRECTORIES
+
+file = open(accessFile, 'r')
+lines = file.readlines()
+
+for num, x in enumerate(lines):
+    if x == '>>> TARGET AlwaysData hg2\n':
+        hosterTarget = x.replace('\n', '')
+        hostTarget = lines[num+1].replace('host:', '').replace('\n', '')
+        portDbTarget = int(lines[num+2].replace('port:', '').replace('\n', ''))
+        userDbTarget = lines[num+3].replace('DB user:', '').replace('\n', '')
+        pwdDbTarget = lines[num+4].replace('DB password:', '').replace('\n', '')
+        nameDbTarget = lines[num+5].replace('DB name:', '').replace('\n', '')
+        prefixTableTarget = lines[num+6].replace('Tables prefix:', '').replace('\n', '')
+        domainTarget = lines[num+7].replace('Domain:', '').replace('\n', '')
+
+        print('\n' + hosterTarget)
+        print('Host target:\t\t\t' + hostTarget)
+        print('Port target:\t\t\t' + str(portDbTarget))
+        print('User DB target:\t\t\t' + userDbTarget)
+        print('Password DB target:\t\t' + pwdDbTarget)
+        print('Name DB target:\t\t\t' + nameDbTarget)
+        print('Prefix table target:\t' + prefixTableTarget)
+        print('Domain target:\t' + domainTarget)
+
+    if x == '>>> SOURCE AlwaysData Courant\n':
+        hosterSource = x.replace('\n', '')
+        hostSource = lines[num + 1].replace('host:', '').replace('\n', '')
+        portDbSource = int(lines[num + 2].replace('port:', '').replace('\n', ''))
+        userDbSource = lines[num + 3].replace('DB user:', '').replace('\n', '')
+        pwdDbSource = lines[num + 4].replace('DB password:', '').replace('\n', '')
+        nameDbSource = lines[num + 5].replace('DB name:', '').replace('\n', '')
+        prefixTableSource = lines[num + 6].replace('Tables prefix:', '').replace('\n', '')
+        domainSource = lines[num+7].replace('Domain:', '').replace('\n', '')
+
+        print('\n' + hosterSource)
+        print('Host source:\t\t\t' + hostSource)
+        print('Port source:\t\t\t' + str(portDbSource))
+        print('User DB source:\t\t\t' + userDbSource)
+        print('Password DB source:\t\t' + pwdDbSource)
+        print('Name DB source:\t\t\t' + nameDbSource)
+        print('Prefix table source:\t' + prefixTableSource)
+        print('Domain source:\t' + domainSource)
+
+
