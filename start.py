4429c274bb165fb293e89d91ea6ad48a6caf3502
georgie123 <georges.hinot@gmail.com>
2024-07-25 18:48:14 +0200

    Standard update

--- start.py
+++ start.py
@@ -225,6 +225,12 @@ for stepScript in listStepScripts:
 
 
 print(colored('\nEND OF DATA MIGRATION!', 'green'))
+
+print(colored('Please:', 'yellow'))
+print(colored('- Clear cache from the Joomla administration.', 'yellow'))
+print(colored('- Update the database structure from the Joomla administration.', 'yellow'))
+print(colored('- Unlock the database from the Joomla administration.', 'yellow'))
+
 time.sleep(1)
 
 
